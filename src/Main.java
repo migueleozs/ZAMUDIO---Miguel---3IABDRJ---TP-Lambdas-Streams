70fd372a1f4a7494565891aa940e983ff093b0b2
Erick Zamudio <zeds94@gmail.com>
2026-07-18 10:40:28 +0200

    First part completed

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 import factory.TripFactory;
 import models.Trip;
+import exo.*;
 
 void main() {
-    List<Trip> trips = TripFactory.generateTrips(10000000);
+    // List<Trip> trips = TripFactory.generateTrips(10000000);
+
+    List<Trip> trips = TripFactory.generateTrips(10);
+
+    Partie1.run(trips);
+
+
 
     // appeler les méthodes des exos ici
     // pour tester si ça marche bien, générer une liste de 10 éléments et afficher le résultat
--- src/exo/Partie1.java
+++ src/exo/Partie1.java
@@ -2,22 +2,53 @@ package exo;
 
 import models.Trip;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Partie1 {
 
+    Predicate<Trip> longTrip = trip -> trip.distanceKm() > 10;
+    Predicate<Trip> expensiveTrip = trip -> trip.price() > 20;
+    Predicate<Trip> badTrip = trip -> trip.rating() < 3;
+    Predicate<Trip> recentTrip = trip -> {
+        LocalDateTime tripDate = trip.startTime();
+        LocalDateTime today = LocalDateTime.now();
+        LocalDateTime yesterday = today.minusDays(1);
+
+        if (tripDate.isEqual(today) || tripDate.isEqual(yesterday) ) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+
     public List<Trip> longAndExpensiveTrips(List<Trip> trips) {
         // distance > 10km et prix > 20€
-        return List.of();
+        return trips.stream()
+                .filter(longTrip.and(expensiveTrip))
+                .toList();
     }
 
     public List<Trip> badTrips(List<Trip> trips) {
-        // rating < 3
-        return List.of();
+        return trips.stream()
+                .filter(badTrip)
+                .toList();
     }
 
     public List<Trip> recentTrips(List<Trip> trips) {
         // aujourd’hui ou hier
-        return List.of();
+        return trips.stream()
+                .filter(recentTrip)
+                .toList();
     }
+
+
+    public void run (List<Trip> trips) {
+        long start = System.currentTimeMillis();
+
+        List<Trip> LandEtrips = longAndExpensiveTrips(trips);
+    }
+
 }
